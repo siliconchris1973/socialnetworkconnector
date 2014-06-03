2fb2a14f3f573ab2d66289a0c2251cd13d736c39
siliconchris1973 <chris.guenther@mac.com>
2014-06-03 18:33:20 +0200

    Added Foursquere and FinanzForum

--- src/main/java/de/comlineag/sbm/data/SocialNetworks.java
+++ src/main/java/de/comlineag/sbm/data/SocialNetworks.java
@@ -5,13 +5,20 @@ package de.comlineag.sbm.data;
  * @author Christian Guenther, Magnus Leinemann
  * @category data enumeration
  * 
- * @description contains an enumeration with codes and shortcuts referencing the
- *              social networks
+ * @description contains an enumeration with shortcuts referencing the social networks
  * @version 1.0
  * 
  */
 public enum SocialNetworks {
-	TWITTER("TW"), FACBOOKk("FB"), GOOGLE("GO"), LINKEDIN("LI"), XING("XI"), STREAMWORK("SW"), INSTAGRAM("IN");
+	TWITTER("TW"), 
+	FACBOOKk("FB"), 
+	GOOGLE("GO"), 
+	LINKEDIN("LI"), 
+	XING("XI"), 
+	STREAMWORK("SW"), 
+	INSTAGRAM("IN"),
+	FOURSQUARE("FO"),
+	FINANZFORUM("FF");
 
 	private final String value;
 
