b52ad6528bef54fe33f97cca2e98747fc76c735a
siliconchris1973 <chris.guenther@mac.com>
2014-07-31 23:07:40 +0200

    added local repo for ngdbc driver

---
+++ repo/deploy_ngdbc.sh
@@ -0,0 +1,1 @@
+mvn deploy:deploy-file -Durl=file:///Users/chris/Developer/workspace/socialnetworkconnector/repo -Dfile=./ngdbc-1.00.68.jar -DgroupId=com.sap -DartifactId=ngdbc -Dpackaging=jar -Dversion=1.00.68
