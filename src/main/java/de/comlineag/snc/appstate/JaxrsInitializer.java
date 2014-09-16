ab1678da28d100d6109adfa182430a9087791534
siliconchris1973 <chris.guenther@mac.com>
2014-09-17 00:27:07 +0200

    deleted OLD boiler pipe and ceased new

--- src/main/java/de/comlineag/snc/appstate/JaxrsInitializer.java
+++ src/main/java/de/comlineag/snc/appstate/JaxrsInitializer.java
@@ -4,7 +4,4 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
  
 @ApplicationPath("/api/v1")
-public class JaxrsInitializer extends Application{
- 
- 
-}
+public class JaxrsInitializer extends Application{}
--- src/main/java/de/comlineag/snc/helper/BoilerpipeContentExtractionService.java
+++ src/main/java/de/comlineag/snc/helper/BoilerpipeContentExtractionService.java
@@ -40,6 +40,5 @@ public class BoilerpipeContentExtractionService {
         } catch (Exception e) {
             return null;
         }
- 
     }
 }
